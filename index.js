b2b64d553d07729bc568f7ac7b079c12117f8096
lexms <alexandermanuel.s@gmail.com>
2020-07-08 11:52:20 +0700

    predict via live video

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let net;
 const webcamElement = document.getElementById('webcam');
 const inputElement = document.getElementById('input_image');
 
@@ -6,19 +5,18 @@ function preprocessing(image){
     inputTensor = tf.browser.fromPixels(image)
     inputTensor = inputTensor.expandDims(0);
     inputScale = inputTensor.div(255)
-    console.log(inputScale)
+    //console.log(inputScale)
     return inputScale
 }
 async function app() {
-    console.log('Loading model. wait until success message shown');
+    alert('Loading model. wait until success message shown');
     // Load the model.
     const model = await tf.loadLayersModel('json-model-2/model.json')
     model.compile({
         optimizer: 'adam',
         loss: 'binaryCrossentropy',
     })      
-    console.log('Successfully loaded model');
-
+    alert('Successfully loaded model');
 
 
     // Predict via upload
@@ -36,11 +34,11 @@ async function app() {
         data = document.getElementById('output_image')
         input = preprocessing(data)
         prediction = model.predict(input);
-        prediction.print()
+        //prediction.print()
         prediction = prediction.flatten()
         prediction = prediction.arraySync()
         label = tf.where(prediction > 0.5, 1, 0) // 0 Fresh, 1 Rotten
-        label.print()
+        //label.print()
         label =label.arraySync()
 
         if (label == 0) {
@@ -58,19 +56,31 @@ async function app() {
         input.dispose();
     });
 
+    //Predict via video
+    const webcam = await tf.data.webcam(webcamElement);
+    while (true) {
+        let prediction = 0.0
+        captured = preprocessing(webcamElement)
+        prediction = model.predict(captured);
+        prediction = prediction.flatten()
+        prediction = prediction.arraySync()
+        label = tf.where(prediction > 0.5, 1, 0) // 0 Fresh, 1 Rotten
+        label =label.arraySync()
 
-    // Predict via camera
+        if (label == 0) {
+            confident = 1.0-prediction
+            predict_result = 'Fresh Fruit'
+        }else{
+            confident = prediction
+            predict_result = 'Rotten Fruit'
+        }     
+        confident = confident * 100
+        document.getElementById('confident-video').innerText  = confident.toFixed(2) +'%';
+        document.getElementById('prediction-video').innerText  = predict_result;
 
-    // while (true){
-    //     const webcam = await tf.data.webcam(webcamElement);  
-    //     const img = await webcam.capture();
-    //     console.log(img)
-    //     const tensor = await tf.browser.fromPixels(img) 
-        
-    //     const prediction = model.predict(tensor);
-    //     img.dispose();
-    //     await tf.nextFrame();
-    // }
+        captured.dispose();
+        await tf.nextFrame();
+  }
 
 }
 
