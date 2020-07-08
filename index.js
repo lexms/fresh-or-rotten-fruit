7bc9b519d1895551ac1cab1f14ab9ef996755863
lexms <alexandermanuel.s@gmail.com>
2020-07-08 12:28:46 +0700

    live video capture button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const webcamElement = document.getElementById('webcam');
 const inputElement = document.getElementById('input_image');
+const buttonElement = document.getElementById('button-on-off')
 
 function preprocessing(image){
     inputTensor = tf.browser.fromPixels(image)
@@ -9,14 +10,14 @@ function preprocessing(image){
     return inputScale
 }
 async function app() {
-    alert('Loading model. wait until success message shown');
+    //alert('Loading model. wait until success message shown');
     // Load the model.
     const model = await tf.loadLayersModel('json-model-2/model.json')
     model.compile({
         optimizer: 'adam',
         loss: 'binaryCrossentropy',
     })      
-    alert('Successfully loaded model');
+    //alert('Successfully loaded model');
 
 
     // Predict via upload
@@ -57,8 +58,7 @@ async function app() {
     });
 
     //Predict via video
-    const webcam = await tf.data.webcam(webcamElement);
-    while (true) {
+    async function predict_video(){
         let prediction = 0.0
         captured = preprocessing(webcamElement)
         prediction = model.predict(captured);
@@ -78,9 +78,51 @@ async function app() {
         document.getElementById('confident-video').innerText  = confident.toFixed(2) +'%';
         document.getElementById('prediction-video').innerText  = predict_result;
 
+
         captured.dispose();
         await tf.nextFrame();
-  }
+    }
+
+    const webcam = await tf.data.webcam(webcamElement);
+    buttonElement.addEventListener('click', function(){
+        currentvalue = buttonElement.innerText
+        if(currentvalue == "Capture"){
+            buttonElement.innerText = 'Captured'
+            buttonElement.disabled = true
+            setTimeout(function(){
+                buttonElement.innerText = 'Capture'
+                buttonElement.disabled = false
+            },2000)
+          }else{
+            buttonElement.innerText = 'Capture'
+           
+          }
+          predict_video()
+
+    //     while (true) {
+    //         let prediction = 0.0
+    //         captured = preprocessing(webcamElement)
+    //         prediction = model.predict(captured);
+    //         prediction = prediction.flatten()
+    //         prediction = prediction.arraySync()
+    //         label = tf.where(prediction > 0.5, 1, 0) // 0 Fresh, 1 Rotten
+    //         label =label.arraySync()
+    
+    //         if (label == 0) {
+    //             confident = 1.0-prediction
+    //             predict_result = 'Fresh Fruit'
+    //         }else{
+    //             confident = prediction
+    //             predict_result = 'Rotten Fruit'
+    //         }     
+    //         confident = confident * 100
+    //         document.getElementById('confident-video').innerText  = confident.toFixed(2) +'%';
+    //         document.getElementById('prediction-video').innerText  = predict_result;
+    
+    //         captured.dispose();
+    //         await tf.nextFrame();
+    //   }
+    })
 
 }
 
