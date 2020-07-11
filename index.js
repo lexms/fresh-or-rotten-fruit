612c8d07bfd31d6b4ba28fe2f64afd5c84c0e5b5
sitialaw <alawiahsiti13@gmail.com>
2020-07-11 22:23:55 +0700

    animasi loading

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 const webcamElement = document.getElementById('webcam');
 const inputElement = document.getElementById('input_image');
-const buttonElement = document.getElementById('button-on-off')
+const buttonElement = document.getElementById('button-on-off');
+const labelText = document.getElementById('choose_file');
+const sendButton = document.getElementById('send_btn');
 
 function preprocessing(image){
     inputTensor = tf.browser.fromPixels(image)
@@ -10,28 +13,19 @@ function preprocessing(image){
     return inputScale
 }
 async function app() {
+
     //alert('Loading model. wait until success message shown');
-    // Load the model.
-    const model = await tf.loadLayersModel('json-model-2/model.json')
+    const model = await tf.loadLayersModel('../json-model-2/model.json')
     model.compile({
         optimizer: 'adam',
         loss: 'binaryCrossentropy',
     })      
+    console.log("MODEL"+model)
     //alert('Successfully loaded model');
 
-
-    // Predict via upload
-    inputElement.addEventListener('change', function () {
+    sendButton.addEventListener('click', function() {
         let prediction = 0.0
-
-        // add image to preview
-        var reader = new FileReader();
-        reader.onload = function(){
-        var output = document.getElementById('output_image');
-        output.src = reader.result;
-        }
-        reader.readAsDataURL(event.target.files[0]);
-
+        $.LoadingOverlay("show"); //untuk memunculkan
         data = document.getElementById('output_image')
         input = preprocessing(data)
         prediction = model.predict(input);
@@ -53,8 +47,21 @@ async function app() {
         confident = confident * 100
         document.getElementById('confident').innerText  = confident.toFixed(2) +'%';
         document.getElementById('prediction').innerText  = predict_result;
-
         input.dispose();
+        $.LoadingOverlay("hide"); //untuk menutup
+    });
+
+
+    // Predict via upload
+    inputElement.addEventListener('change', function (event) {
+        // add image to preview
+        var reader = new FileReader();
+        reader.onload = function(){
+            var output = document.getElementById('output_image');
+            output.src = reader.result;
+        }
+        reader.readAsDataURL(event.target.files[0]);
+        labelText.innerHTML = event.target.files[0].name;
     });
 
     //Predict via video
@@ -85,6 +92,7 @@ async function app() {
 
     const webcam = await tf.data.webcam(webcamElement);
     buttonElement.addEventListener('click', function(){
+        $.LoadingOverlay("show"); //untuk memunculkan
         currentvalue = buttonElement.innerText
         if(currentvalue == "Capture"){
             buttonElement.innerText = 'Captured'
@@ -98,7 +106,7 @@ async function app() {
            
           }
           predict_video()
-
+         $.LoadingOverlay("hide"); //untuk memunculkan
     //     while (true) {
     //         let prediction = 0.0
     //         captured = preprocessing(webcamElement)
@@ -123,7 +131,7 @@ async function app() {
     //         await tf.nextFrame();
     //   }
     })
-
+    
 }
 
 app();
